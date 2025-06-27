61fbc816d39bee5bbe14138cb133f7e6d1e4740d
shubhamsfit <shubham.kohli@simplefixit.com>
2025-06-27 17:42:59 +0530

    updated files

--- backend_tests/test_api.py
+++ backend_tests/test_api.py
@@ -2,7 +2,7 @@ import requests
 
 def test_api_status_code():
     res = requests.get("https://jsonplaceholder.typicode.com/posts/1")
-    assert res.status_code == 200
+    assert res.status_code == 500
 
 def test_api_data_content():
     res = requests.get("https://jsonplaceholder.typicode.com/posts/1")
